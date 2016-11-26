ba1485895d329f422a5447b293d36d7786afd585
Sidney de Moraes <bitbucket@najjah.33mail.com>
2016-11-26 19:11:31 -0200

    Configuration added to redirect 404 errors to home page.

--- src/main/groovy/br/com/smc/jsondiff/Application.groovy
+++ src/main/groovy/br/com/smc/jsondiff/Application.groovy
@@ -2,6 +2,11 @@ package br.com.smc.jsondiff
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer
+import org.springframework.boot.web.servlet.ErrorPage
+import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpStatus
 
 /**
  * Spring Boot initialization class.
@@ -9,8 +14,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class Application {
 
+	/**
+	 *  Initialization method
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args)
+	}
+
+	/**
+	 * Configuration that customizes 404 errors by redirecting them to the main home page
+	 * where information about usage of the service can be found.
+	 */
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/");
+				container.addErrorPages(error404Page);
+			}
+		};
 	}
 }
 
