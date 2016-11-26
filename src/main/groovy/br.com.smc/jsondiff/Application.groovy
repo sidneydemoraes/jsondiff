5c7c3090d4803671d2d0ae55c0fefb4e37f94f99
Sidney de Moraes <bitbucket@najjah.33mail.com>
2016-11-26 17:53:53 -0200

    Added Spring Boot initialization class.

---
+++ src/main/groovy/br.com.smc/jsondiff/Application.groovy
@@ -0,0 +1,16 @@
+package br.com.smc.jsondiff
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * Spring Boot initialization class.
+ */
+@SpringBootApplication
+class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args)
+	}
+}
+
